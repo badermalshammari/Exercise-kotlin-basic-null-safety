fd5d1ac59c907f039aaaf138fee7c0ce3347cb79
Bader Alshammari <q8.badoori.11@gmail.com>
2025-02-18 18:41:24 +0300

    null safety

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,25 @@
+fun evaluateScore(score: Double?) {
+    val result = if (score != null) {
+        if (score >= 50) "Pass" else "Fail"
+    } else {
+        "No Score Provided"
+    }
+
+    println(result)
+}
+
 fun main() {
-    println("Hello World!")
+    var age: Int = 25
+//    age = null
+    println(age)
+
+    var city : String? = "Madrid"
+    city = null
+    println(city)
+    println(city?.length)
+    val cityLength = city?.length ?: 0
+    println(cityLength)
+
+    evaluateScore(null)
+
 }
